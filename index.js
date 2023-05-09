bced1d1126d5ea71223aa59445a76bd1509ffd5c
anastafish <anastafish24@gmail.com>
2023-05-10 00:23:16 +0300

    Request Header Parser Microservice

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+const os = require('os')
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -23,6 +24,10 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/whoami', (req, res) => {
+  res.json({ipaddress:req.ip, language:req.headers['accept-language'], software:req.headers['user-agent']})
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
